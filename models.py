7c7ac0e299513b544ab8771d59973c795d265203
msenzanje <ksenzanje@gmail.com>
2025-04-04 22:51:50 -0500

    chat.py - Implement chat.py to support basic prompt handling for chat functionality - Minor changes

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from langchain_ollama import OllamaEmbeddings, ChatOllama
 
 class Models: 
     def __init__(self):
-              # Snowflake embedding model, BERT arch
+        # Snowflake embedding model, BERT arch
         self.embeddings_ollama = OllamaEmbeddings(
             model = 'snowflake-arctic-embed:22m',
         )
