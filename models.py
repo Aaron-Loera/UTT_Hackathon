20ad09fc573a8cdcaa6877e3946963e8f9b9e9b4
Aaron Loera <loeraaaron776@gmail.com>
2025-04-04 22:35:47 -0500

    Updated referenced embedding model to "snowflake-arctic-embed:22m"

--- models.py
+++ models.py
@@ -3,5 +3,5 @@ from langchain_ollama import OllamaEmbeddings, ChatOllama
 class Models:
     
     def __init__(self):
-        self.embeddings_ollama = OllamaEmbeddings(model='snowflake-arctic-embed')
+        self.embeddings_ollama = OllamaEmbeddings(model='snowflake-arctic-embed:22m')
         self.model_ollama = ChatOllama(model='llama3.2:latest')
