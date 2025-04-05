7753de9f9ab65bbdc9b6e8a8726f292ed58eb62f
Aaron Loera <loeraaaron776@gmail.com>
2025-04-05 02:17:09 -0500

    Reduced relevant documents to 5 and printed indiviudal documents used for context

--- chat.py
+++ chat.py
@@ -21,11 +21,11 @@ prompt = ChatPromptTemplate.from_messages([("system", "You are a knowledgable an
                                             the provided context. If the context is not sufficient, say I need more information to \
                                             answer this. Do not make up information."),
                                            
-                                            ("human", "Use the user input {input} to answer question. Use {context} to answer question.")])
+                                            ("human", "Answer the question: {input}\nContext:{context}")])
 
 # Define the retrieval chain
-# Retriver from our vector store. To find and retrieve relevent docs
-retriever = vector_store.as_retriever(search_kwargs={"k":10}) # 10 most relevant docs
+# Retriever from our vector store. To find and retrieve relevent docs
+retriever = vector_store.as_retriever(search_kwargs={"k":5}) # 10 most relevant docs
 combine_docs_chain = create_stuff_documents_chain(llm, prompt)
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
@@ -36,8 +36,14 @@ def main():
         query = input("Please provide a question you need help with. Otherwise enter 'q' or 'quit' to end the conversation: ")
         if query.lower() in ['q', 'quit']:
             break
+        
         result = retrieval_chain.invoke({"input": query})
-        print(f"Retrieved Context: {result["context"]}\n")
+        
+        # Retrieveing and printing the documents the LLM finds relevant
+        docs = retriever.get_relevant_documents(query)
+        for i, doc in enumerate(docs):
+            print(f"Doc {i+1}: \n{doc.page_content}\n")
+            
         print(f"Assistant: {result.get("answer", "Sorry, I couldn't find a helpful response.")} \n\n")
 
 # Run main loop
