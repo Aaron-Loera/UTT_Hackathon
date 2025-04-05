b18be443f59874a581181322b29581ef2654dc49
Aaron Loera <loeraaaron776@gmail.com>
2025-04-04 23:20:03 -0500

    Updated documentation in "chat.py"

--- chat.py
+++ chat.py
@@ -4,40 +4,37 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_chroma import Chroma as CDb
 
-# Initialize models
+# Initialize both LLM model and Embedding model
 models =  Models()
 embeddings  = models.embeddings_ollama
 llm = models.model_ollama
 
-# Vector store
-vector_store  = CDb(
+# Storing location for vector database
+vector_store = CDb(
     collection_name="documents",
     embedding_function=embeddings,
     persist_directory="./db/chroma_langchain_db"  # Vector store location
 )
 
-# Template used to guide esponse
+# Template used to guide response
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helfpful college advisor. Answer the questions based on data provided"),
-        ("human", "Use the user input {input} to answer question. Use {context} to"
-        "answer question")
+        ("system", "You are a helfpful college advisor. Answer the questions based on data provided."),
+        ("human", "Use the user input {input} to answer question. Use {context} to answer question.")
     ]
 )
 
 # Define the retrieval chain
 # Retriver from our vector store. To find and retrieve relevent docs
-retriever = vector_store.as_retriever(kwargs={"k":10}) # 10 most relevant docs
-combine_docs_chain = create_stuff_documents_chain(
-    llm, prompt
-)
+retriever = vector_store.as_retriever(search_kwargs={"k":10}) # 10 most relevant docs
+combine_docs_chain = create_stuff_documents_chain(llm, prompt)
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
 
 # Main loop
 def main():
     while True:
-        query = input("'q' or 'quit to end: ")
+        query = input("'q' or 'quit' to end: ")
         if query.lower() in ['q', 'quit']:
             break
         result = retrieval_chain.invoke({"input": query})
