c8b263dd45dbcd2a0002fef3f922f714f5d6a5bc
MatthewE <mattiec2016@gmail.com>
2025-04-05 00:51:34 -0500

    got the matching data for profs

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -31,8 +31,27 @@ for card in cards:
     rating_tag = card.find("div", class_=re.compile("CardNumRating__CardNumRatingNumber"))
     rating = float(rating_tag.text.strip()) if rating_tag else None
 
-    data.append((name, rating))
+    # get department
+    department_tag = card.find("div", class_="CardSchool__Department-sc-19lmz2k-0 hRJPlj")
+    department = department_tag.text.strip() if department_tag else "No Name"
 
-for name, rating in data:
-    print(f"{name}: {rating}")
+    #get number of reviews 
+    numRatings_tag = card.find("div", class_=("CardNumRating__CardNumRatingCount-sc-17t4b9u-3 ckSFVh"))
+    department = numRatings_tag.text.strip() if numRatings_tag else "No Ratings"
+
+    # get feedback numbers
+    feedback_tags = card.find_all("div", class_="CardFeedback__CardFeedbackNumber-lq6nix-2 iHkSBk")
+    retake = feedback_tags[0].text.strip() if len(feedback_tags) > 0 else "No % given"
+    difficulty = feedback_tags[1].text.strip() if len(feedback_tags) > 1 else "No difficulty given"
+
+
+
+
+
+
+
+    data.append((name, rating, department, rating, retake, difficulty))
+
+for name, rating, department, rating, retake, difficulty in data:
+    print(f"{name}:  {rating}, {department}, {rating}, {retake}, {difficulty}")
 
